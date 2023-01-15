e8d860c9162c3d3782d59242d11db6ce09ae5564
Colin Nielsen <colinrnielsen@gmail.com>
2023-01-14 21:17:42 -0700

    feat: :safety_vest: Web3Enabled wrapper component

---
+++ frontend/components/utility/Web3Enabled.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { isConnected, useWeb3 } from "../state/useWeb3";
+
+export const Web3Enabled = ({
+  children,
+  fallback,
+}: {
+  children: React.ReactNode;
+  fallback?: React.ReactNode;
+}) => {
+  const web3 = useWeb3();
+
+  return <>{isConnected(web3) ? children : fallback}</>;
+};
